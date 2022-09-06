48c4371f72d4848e5f8c919c512856750b0063c0
mcesmeralda <emontoya.joma@gmail.com>
2022-09-06 01:05:45 -0500

    avanze reto

---
+++ Java/reto_1/src/reto_1.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class reto_1 {
+    public static void main(String[] args) {
+        int Array[] = {1,2,3,4,5,10,25};
+
+        int j[] = {20};
+        int i[] = {Array.length,20,Array.length};
+
+        System.out.println(Arrays.toString(i));
+    }
+}
