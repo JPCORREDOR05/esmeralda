e70e354d5afb7ad3707f62ae3888ac0506d0761f
mcesmeralda <emontoya.joma@gmail.com>
2022-09-06 23:23:17 -0500

    terminado reto1

--- Java/reto_1/src/reto_1.java
+++ Java/reto_1/src/reto_1.java
@@ -1,11 +1,24 @@
 import java.util.Arrays;
 public class reto_1 {
     public static void main(String[] args) {
-        int Array[] = {1,2,3,4,5,10,25};
+        int reporte[] = {1,2,3,4,5,10,25};
 
+        int length = reporte.length;
+        for (int i = 0; i < length; i++ ){
+        }
+        int mayor, minor;
+        mayor = minor = reporte[0];
+        for (int i = 0; i<reporte.length; i++){
+            if(reporte [i] >mayor) {
+                mayor = reporte[i];
+            }
+            if(reporte [i] <minor) {
+                minor = reporte[i];
+            }
+        }
         int j[] = {20};
-        int i[] = {Array.length,20,Array.length};
+        int w[] = {reporte.length,minor,mayor};
 
-        System.out.println(Arrays.toString(i));
+        System.out.println(Arrays.toString(w));
     }
 }
